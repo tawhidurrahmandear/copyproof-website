87bb7b5f174fc04b442d19780c37ea4e9f054ed7
Tawhidur Rahman Dear <114006190+tawhidurrahmandear@users.noreply.github.com>
2024-12-29 21:52:07 +0600

    Delete v1.0 directory

--- v1.0/media/plg_system_copyproof_website/copyproof_website.js
+++
@@ -1,6 +0,0 @@
-// Copyproof Joomla Website | Joomla Extension
-// Download from https://dearstore.gumroad.com/l/Copyproof-Joomla-Website 
-// Copyright (C) Tawhidur Rahman Dear. All rights reserved.
-// License - GNU General Public License version 2 or later; see LICENSE.txt
-
-function disableSelection(e){if(typeof e.onselectstart!="undefined")e.onselectstart=function(){return false};else if(typeof e.style.MozUserSelect!="undefined")e.style.MozUserSelect="none";else e.onmousedown=function(){return false};e.style.cursor="default"}window.onload=function(){disableSelection(document.body)}
